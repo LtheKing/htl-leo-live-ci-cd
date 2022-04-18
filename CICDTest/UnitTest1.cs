1d0bf36e0f477faccc577218b7bb6a3492f35928
Leonaldi Nata Gunawan <leonalding77@gmail.com>
2022-04-19 00:14:37 +0700

    add new test

--- CICDTest/UnitTest1.cs
+++ CICDTest/UnitTest1.cs
@@ -1,14 +1,13 @@
-using System;
 using Xunit;
 
-namespace TestProject1
+namespace CICDTest
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-            Assert.True(1 == 1);
+
         }
     }
 }
